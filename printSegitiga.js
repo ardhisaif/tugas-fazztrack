24c6920a0d56cc48b1620eb1fe4410678583bdf8
unknown <ahmadsaifudinardhiansyah@gmail.com>
2022-12-12 08:14:18 +0700

    week1

---
+++ printSegitiga.js
@@ -0,0 +1,10 @@
+const printSegitiga = 6
+
+for (let i = printSegitiga; i > 0; i--) {
+    let baris = ""
+    for (let j = 1 ; j <= i; j++) {
+        baris += j 
+    }
+    console.log(baris);
+    baris =""
+}
